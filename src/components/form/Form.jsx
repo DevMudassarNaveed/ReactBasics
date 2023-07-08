ac03246e2b5d41aaf1ea551a063ccff7b2569049
devmmudassar <devmmudassar@gmail.com>
2023-07-08 10:52:41 -0700

    Added logic

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,8 +4,17 @@ import { FormControl, InputLabel, Input,Button } from '@mui/material';
 import { FormWrapper } from "./form.style";
 
 const InputForm = () => {
-    const [item, setItem] = useState("");
-    console.log('item', item);
+    const [item,setItem ] = useState("");
+    const [todos, setTodos] = useState([]); // intilizing it with an empty array as we have many todos
+    const handleClick = () =>{
+    setTodos((currentItems)=>
+    {
+        return[...currentItems  , {title: item , status: false}];
+
+    })
+    setItem("");
+    }
+  
     return (
         <>
             <FormWrapper>
@@ -15,15 +24,21 @@ const InputForm = () => {
                 </FormControl>
 
             </FormWrapper>
-            <FormWrapper>  <Button variant="contained">Contained</Button>  </FormWrapper>
+            <FormWrapper>  <Button variant="contained" onClick={handleClick}>Contained</Button>  </FormWrapper>
                 <FormWrapper>
                 <ul>
-                    <li>
-                        <label>
-                            <input type="checkbox"/>
-                            Item 1
-                        </label>
-                    </li>
+                    
+                        {todos.map((list, id)=>(
+                            <li key={id}>
+                              <label>
+                              <input type="checkbox" checked={list.status}/>
+                                    {list.title}
+                          </label>
+                          
+</li>
+                        ))}
+                      
+                   
                 </ul>
             </FormWrapper>
         </>
